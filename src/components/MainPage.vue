f2195f156aabe9f71b631853e7e982aaa6a4d688
Russell Lin <superbr7789@gmail.com>
2023-05-21 23:16:29 +0800

    fulfill the 2/3 index

---
+++ src/components/MainPage.vue
@@ -0,0 +1,249 @@
+<template>
+  <div>
+    <div class="flex justify-center h-full w-full bg-black">
+      <div class="w-[80vw]">
+        <div
+          class="flex w-full py-5 border-b border-solid border-white justify-between"
+        >
+          <div class="flex items-center">
+            <div>
+              <img
+                src="../assets/Union.png"
+                class="w-[40px] h-[40px]"
+                alt="icon"
+              />
+            </div>
+            <div>
+              <img
+                src="../assets/AI工具王.png"
+                class="h-[40px] ml-[14px]"
+                alt="ai-tool"
+              />
+            </div>
+          </div>
+          <ul class="flex items-center">
+            <li class="font-bold text-base">首頁</li>
+            <li class="font-bold text-base ml-5">定價</li>
+          </ul>
+        </div>
+        <div class="w-full">
+          <div class="py-10">
+            <div class="font-black text-[120px] leading-[144px]">透過</div>
+            <div class="font-black text-[120px] leading-[144px]">
+              AI工具王的
+            </div>
+            <div class="font-black text-[120px] leading-[144px]">強大模型</div>
+            <div class="font-black text-[120px] leading-[144px]">
+              讓您的業務更聰明
+            </div>
+          </div>
+          <div>
+            <button
+              class="border border-solid border-white text-[20px] leading-[24px] font-black px-10 py-5 rounded-2xl"
+            >
+              開始使用
+            </button>
+          </div>
+          <div class="flex w-full justify-end pt-[100px]">
+            <div>
+              <img
+                src="../assets/3.png"
+                alt="anime-icon-1"
+                class="w-[80px] h-[80px]"
+              />
+            </div>
+            <div>
+              <img
+                src="../assets/3.png"
+                alt="anime-icon-2"
+                class="w-[80px] h-[80px]"
+              />
+            </div>
+            <div>
+              <img
+                src="../assets/3.png"
+                alt="anime-icon-3"
+                class="w-[80px] h-[80px]"
+              />
+            </div>
+          </div>
+          <div class="w-full py-[160px]">
+            <div class="flex justify-between">
+              <div class="w-[30%] section-title">我們的服務</div>
+              <div class="w-[65%]">
+                <div class="font-black text-5xl">
+                  我們相信透過 AI 技術，<br />包括圖像識別、自然語言處理和機器翻譯等，讓您更快、更準確地做出決策。
+                </div>
+              </div>
+            </div>
+            <div class="flex mt-20 justify-between">
+              <div
+                class="py-20 px-10 w-[32%] border border-solid border-[#525252]/80 rounded-2xl"
+              >
+                <div class="w-[80px] h-[80px] px-5">
+                  <img src="../assets/Vector.png" alt="rapid" />
+                </div>
+                <div class="font-black text-[32px] leading-[38.4px] my-5">
+                  快速
+                </div>
+                <div class="text-base text-[#f2f2f2]">
+                  我們先進的推理基礎設施提供了極短的響應時間，讓你的產品服務不間斷。
+                </div>
+              </div>
+              <div
+                class="py-20 px-10 w-[32%] border border-solid border-[#525252]/80 rounded-2xl"
+              >
+                <div class="w-[80px] h-[80px] px-1">
+                  <img src="../assets/Vector_1.png" alt="agile" />
+                </div>
+                <div class="font-black text-[32px] leading-[38.4px] my-5">
+                  靈活
+                </div>
+                <div class="text-base text-[#f2f2f2]">
+                  我們的服務可以根據您的需求進行靈活調整，同時還可以滿足不同項目和預算的需求。
+                </div>
+              </div>
+              <div
+                class="py-20 px-10 w-[32%] border border-solid border-[#525252]/80 rounded-2xl"
+              >
+                <div class="w-[80px] h-[80px] px-1">
+                  <img src="../assets/Vector_2.png" alt="extendable" />
+                </div>
+                <div class="font-black text-[32px] leading-[38.4px] my-5">
+                  擴充
+                </div>
+                <div class="text-base text-[#f2f2f2]">
+                  我們的服務支持多種擴充選項，可以滿足您的不斷增長的 AI 需求。
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="pb-[160px]">
+            <div class="section-title">來自合作夥伴</div>
+            <div class="mt-20">
+              <div>
+                <img
+                  src="../assets/Logo1.png"
+                  alt="logo-section-one"
+                  class="h-[40px]"
+                />
+              </div>
+              <div class="pt-5 pb-[160px]">
+                <img
+                  src="../assets/Logo2.png"
+                  alt="logo-section-two"
+                  class="h-[40px]"
+                />
+              </div>
+            </div>
+            <div class="flex justify-between">
+              <div class="p-10 w-[32%] rounded-2xl bg-[#1E1E1E]">
+                <div class="flex">
+                  <div class="w-3.5 h-3.5 p-[1px]">
+                    <img src="../assets/Star.png" alt="star-icon" />
+                  </div>
+                  <div class="w-3.5 h-3.5 p-[1px]">
+                    <img src="../assets/Star.png" alt="star-icon" />
+                  </div>
+                  <div class="w-3.5 h-3.5 p-[1px]">
+                    <img src="../assets/Star.png" alt="star-icon" />
+                  </div>
+                  <div class="w-3.5 h-3.5 p-[1px]">
+                    <img src="../assets/Star.png" alt="star-icon" />
+                  </div>
+                  <div class="w-3.5 h-3.5 p-[1px]">
+                    <img src="../assets/Star.png" alt="star-icon" />
+                  </div>
+                </div>
+                <p class="w-full min-h-[215px] my-5 font-normal text-base">
+                  非常喜歡 AI工具王 提供的 AI
+                  模型租賃服務，使用起來非常方便，而且效果非常好。對於我們公司的業務來說，這個服務非常重要，因為我們需要大量的數據進行分析和預測，AI工具王
+                  提供的 AI 模型能夠幫助我們節省大量時間和成本，實在是太棒了！
+                </p>
+                <div class="flex">
+                  <div class="w-[48px] h-[48px] rounded-full">
+                    <img src="../assets/missChen.png" alt="ms-chen-avatar" />
+                  </div>
+                  <div class="ml-4">
+                    <div>陳小姐</div>
+                    <div>ABC科技有限公司</div>
+                  </div>
+                </div>
+              </div>
+              <div class="p-10 w-[32%] rounded-2xl bg-[#1E1E1E]">
+                <div class="flex">
+                  <div class="w-3.5 h-3.5 p-[1px]">
+                    <img src="../assets/Star.png" alt="star-icon" />
+                  </div>
+                  <div class="w-3.5 h-3.5 p-[1px]">
+                    <img src="../assets/Star.png" alt="star-icon" />
+                  </div>
+                  <div class="w-3.5 h-3.5 p-[1px]">
+                    <img src="../assets/Star.png" alt="star-icon" />
+                  </div>
+                  <div class="w-3.5 h-3.5 p-[1px]">
+                    <img src="../assets/Star.png" alt="star-icon" />
+                  </div>
+                  <div class="w-3.5 h-3.5 p-[1px]">
+                    <img src="../assets/Star.png" alt="star-icon" />
+                  </div>
+                </div>
+                <p class="w-full min-h-[215px] my-5 font-normal text-base">
+                  作為一家醫療器材公司，我們非常關注人工智能技術的應用，而
+                  AI工具王 提供的 AI
+                  模型租賃服務為我們帶來了很大的幫助。我們使用 AI工具王
+                  的模型進行醫學影像分析和預測，這些模型非常精確，能夠幫助我們更好地診斷病情，提高治療效果。感謝
+                  AI工具王 團隊的辛勤工作！
+                </p>
+                <div class="flex">
+                  <div class="w-[48px] h-[48px] rounded-full">
+                    <img src="../assets/missLiu.png" alt="ms-liu-avatar" />
+                  </div>
+                  <div class="ml-4">
+                    <div>劉小姐</div>
+                    <div>XYZ 醫療器材有限公司</div>
+                  </div>
+                </div>
+              </div>
+              <div class="p-10 w-[32%] rounded-2xl bg-[#1E1E1E]">
+                <div class="flex">
+                  <div class="w-3.5 h-3.5 p-[1px]">
+                    <img src="../assets/Star.png" alt="star-icon" />
+                  </div>
+                  <div class="w-3.5 h-3.5 p-[1px]">
+                    <img src="../assets/Star.png" alt="star-icon" />
+                  </div>
+                  <div class="w-3.5 h-3.5 p-[1px]">
+                    <img src="../assets/Star.png" alt="star-icon" />
+                  </div>
+                  <div class="w-3.5 h-3.5 p-[1px]">
+                    <img src="../assets/Star.png" alt="star-icon" />
+                  </div>
+                  <div class="w-3.5 h-3.5 p-[1px]">
+                    <img src="../assets/Star.png" alt="star-icon" />
+                  </div>
+                </div>
+                <p class="w-full min-h-[215px] my-5 font-normal text-base">
+                  我們銀行一直在尋找能夠幫助我們提高風險控制和客戶服務的解決方案，而
+                  AI工具王 提供的 AI 模型租賃服務正好滿足了我們的需求。我們使用
+                  AI工具王
+                  的模型進行客戶信用評估和詐騙檢測等任務，這些模型非常準確，能夠幫助我們更好地控制風險，提高客戶滿意度。非常感謝
+                  AI工具王 團隊的支持！
+                </p>
+                <div class="flex">
+                  <div class="w-[48px] h-[48px] rounded-full">
+                    <img src="../assets/mrHuang.png" alt="mr-huang-avatar" />
+                  </div>
+                  <div class="ml-4">
+                    <div>黃先生</div>
+                    <div>EFG銀行</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
