8f65bb9a7b4a8e640402983e5dc6d9201322d777
Russell Lin <superbr7789@gmail.com>
2023-06-04 19:23:39 +0800

    correct the base url setting

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import eslintPlugin from "vite-plugin-eslint";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), eslintPlugin({ cache: false })],
-  base: "ai_tools/",
+  base: "/ai_tools/",
   resolve: {
     alias: {
       "@": resolve(__dirname, "src"),
